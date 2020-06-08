fb3f9dfffb61ac9d8d6e0a545c077b1945897906
Philip Jacobson <philip@abico.se>
2020-06-08 15:34:37 +0200

    uppdate

---
+++ Bostadsvaljare/Data/Houses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bostadsvaljare.Data
+{
+    public class Houses
+    {
+        public double Sqm { get; set; }
+        public string Price { get; set; }
+        public double LandArea { get; set; }
+        public string HouseNumber { get; set; }
+        public string Status { get; set; }
+    }
+        
+}
