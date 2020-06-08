c21de114c195d7fcb440772b3862d3df413a3658
Mark Tibblin <mark.tibblin@gmail.com>
2020-06-08 12:00:39 +0200

    Removed unused files, and renamed existing ones.

--- Bostadsvaljare/Startup.cs
+++ Bostadsvaljare/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Bostadsvaljare.Data;
 using Syncfusion.Blazor;
 using BlazorStrap;
 
@@ -32,7 +31,6 @@ namespace Bostadsvaljare
             services.AddSyncfusionBlazor();
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             
         }
 
