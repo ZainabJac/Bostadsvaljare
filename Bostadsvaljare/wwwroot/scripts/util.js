a8cba3c52031ea7f9ea6155e880f8cc04247479a
Mark Tibblin <mark.tibblin@gmail.com>
2020-06-11 12:43:59 +0200

    Added js script for specific image map code

--- Bostadsvaljare/wwwroot/scripts/util.js
+++ Bostadsvaljare/wwwroot/scripts/util.js
@@ -28,54 +28,6 @@
             }
         },
 
-        addMapHighlights: function () {
-            var colors = {
-                available: '5bcb24',
-                booked: 'dfd431',
-                sold: 'd32626',
-            };
-
-            var options = {
-                staticState: false,
-                fill: true,
-                mapKey: 'status',
-                wrapClass: 'center',
-            };
-
-            var initialOptions = options;
-            initialOptions.onConfigured = function (success) {
-                if (success) {
-                    var statuses = { available: 0, booked: 0, sold: 0 };
-                    options.areas = [];
-
-                    var mapAreas = $('#housing_map')[0].areas
-                    for (var i = 0; i < mapAreas.length; i++) {
-                        var area = mapAreas[i];
-                        var status = area.attributes.status.value;
-                        area.attributes.status.value = status + statuses[status];
-                        options.areas.push({
-                            key: status + statuses[status],
-                            fillColor: colors[status],
-                        });
-                        console.log(status + statuses[status]);
-                        statuses[status] += 1;
-                    }
-
-                    this.mapster('rebind', options);
-
-                    this.css({
-                        width: '100%',
-                    });
-                }
-            };
-
-            $('#overview').mapster(initialOptions);
-        },
-
-        changeMapImage: function (imagePath) {
-            $('img.mapster_el')[0].src = imagePath;
-        },
-
         showTooltip: function (positionX, positionY, msg) {
             var tooltip = $('#tipmsg');
             tooltip.text(msg);
@@ -87,6 +39,6 @@
 
         hideTooltip: function () {
             $('#tipmsg').hide();
-        }
+        },
     };
 })();
