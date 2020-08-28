d288dcf8b092a8f003916cab7d839d91796a9719
Philip Jacobson <Philip@Visivo.se>
2020-08-28 16:33:18 +0200

    Uppdated layput with cards in naromrode. added  a second searchbar. uppdated googlemaps script

--- Bostadsvaljare/wwwroot/scripts/googlemaps.js
+++ Bostadsvaljare/wwwroot/scripts/googlemaps.js
@@ -38,13 +38,18 @@
             // Create the search box and link it to the UI element.
             var input = document.getElementById('pac-input');
             var searchBox = new google.maps.places.SearchBox(input);
+            var input2 = document.getElementById('pac-input2');
+            var searchBox2 = new google.maps.places.SearchBox(input2);
 
             // Bias the SearchBox results towards current map's viewport.
             map.addListener('bounds_changed', function () {
                 searchBox.setBounds(map.getBounds());
+                searchBox2.setBounds(map.getBounds());
             });
 
+
             var markers = [];
+            var markers2 = [];
             googlemaps.directionsDisplay = new google.maps.DirectionsRenderer({
                 suppressMarkers: true,
                 suppressBicyclingLayer: true,
@@ -52,6 +57,76 @@
             googlemaps.directionsDisplay.setMap(map); // map should be already initialized.
             // Listen for the event fired when the user selects a prediction and retrieve
             // more details for that place.
+
+            searchBox2.addListener('places_changed', function () {
+                var places = searchBox2.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+
+                // Clear out the old markers.
+                markers2.forEach(function (marker) {
+                    marker.setMap(null);
+                });
+                markers2 = [];
+
+                // For each place, get the icon, name and location.
+                var bounds = new google.maps.LatLngBounds();
+                places.forEach(function (place) {
+                    if (!place.geometry) {
+                        console.log("Returned place contains no geometry");
+                        return;
+                    }
+                    var icon = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+                    };
+
+                    // Create a marker for each place.
+                    markers2.push(new google.maps.Marker({
+                        map: map,
+                        icon: icon,
+                        title: place.name,
+                        position: place.geometry.location,
+                    }));
+
+                    if (place.geometry.viewport) {
+                        // Only geocodes have viewport.
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        bounds.extend(place.geometry.location);
+                    }
+
+                 
+
+                    var service = new google.maps.DistanceMatrixService();
+                    service.getDistanceMatrix({
+                        origins: [place.geometry.location],
+                        destinations: [destination],
+                        travelMode: googlemaps.request.travelMode
+                    }, callback);
+
+                    function callback(response, status) {
+                        if (status == 'OK') {
+                            var origins = response.originAddresses;
+                            var destinations = response.destinationAddresses;
+
+                            for (var i = 0; i < origins.length; i++) {
+                                var results = response.rows[i].elements;
+                                for (var j = 0; j < results.length; j++) {
+                                    var element = results[j];
+                                }
+                            }
+                        }
+                    }
+                });
+                map.fitBounds(bounds);
+            });
+
             searchBox.addListener('places_changed', function () {
                 var places = searchBox.getPlaces();
 
@@ -136,9 +211,9 @@
                     // Show travel data
                     var routeLeg = response.routes[0].legs[0];
                     var travelDistance = document.getElementById('travel-distance');
-                    travelDistance.innerHTML = routeLeg.distance.text;
+                    travelDistance.innerHTML = "Distans: " + routeLeg.distance.text;
                     var travelTime = document.getElementById('travel-time');
-                    travelTime.innerHTML = routeLeg.duration.text;
+                    travelTime.innerHTML = "Restid: " + routeLeg.duration.text ;
                 }
             });
         },
@@ -153,6 +228,9 @@
                     break;
                 case 'public':
                     googlemaps.request.travelMode = google.maps.TravelMode.TRANSIT;
+                    break;
+                case 'walk':
+                    googlemaps.request.travelMode = google.maps.TravelMode.WALKING;
                     break;
             }
             if (googlemaps.displayingDirections)
