eefc2a8fcc4b785cee792c3930b6c4be8d9a5195
Mark Tibblin <mark.tibblin@gmail.com>
2020-08-29 00:24:01 +0200

    Renamed to singular house, also added a Rent property

--- Bostadsvaljare/Data/House.cs
+++ Bostadsvaljare/Data/House.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 
 namespace Bostadsvaljare.Data
 {
-    public class Houses
+    public class House
     {
         public int ID { get; set; }
         public int Area { get; set; }
         public string Housetype { get; set; }
         public double Sqm { get; set; }
         public string Price { get; set; }
+        public string Rent { get; set; }
         public double LandArea { get; set; }
         public string HouseNumber { get; set; }
         public string Status { get; set; }
