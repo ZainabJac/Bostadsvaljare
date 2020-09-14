b922bae6d2af175f759765e44e467959eb269434
Mark Tibblin <mark.tibblin@gmail.com>
2020-09-14 13:56:03 +0200

    Uses image map to draw circles instead

--- Bostadsvaljare/wwwroot/scripts/interior.js
+++ Bostadsvaljare/wwwroot/scripts/interior.js
@@ -1,7 +1,6 @@
 ﻿(function () {
     window.interior = {
         onResize: function (event) {
-            //TODO: change hotspots' positions to follow the image's size
             if ($(window).width() <= 927) {
                 var height = parseInt($('.planritning').height());
                 $('#gallery').height((height + 3) + 'px');
@@ -12,18 +11,6 @@
 
         addResizeListener: function () {
             window.addEventListener('resize', this.onResize, false);
-        },
-
-        setHotspot: function (parentID, aID, x, y, r) {
-            var a = $(parentID +' '+ aID),
-                img = $(parentID +' img');
-
-            x = x * (img.width() / img[0].naturalWidth) - r;
-            y = y * (img.height() / img[0].naturalHeight) - r;
-            a.css({
-                left: x + 'px',
-                top: y + 'px',
-            });
         },
 
         dispose: function () {
