d0eed23b18f81a2cce7332887e9ec5d67c9ca99d
Mark Tibblin <mark.tibblin@gmail.com>
2020-09-15 13:56:06 +0200

    Skip handling of fullscreenchange event

--- Bostadsvaljare/wwwroot/scripts/bv.js
+++ Bostadsvaljare/wwwroot/scripts/bv.js
@@ -1,27 +1,22 @@
 ﻿(function () {
     window.bv = {
-        fullscreen: false,
+        isFullscreen: false,
         listeners: {},
 
         initialize: function () {
             var self = this;
 
-            this.listeners.fullscreenchange = function(e) { self._onFullscreenChange(e); };
             this.listeners.toggleFullscreen = function() { self.toggleFullscreen(); };
 
-            document.addEventListener('fullscreenchange', this.listeners.fullscreenchange, false);
             $('#bv-fullscreen-btn').on('click', this.listeners.toggleFullscreen);
         },
 
         toggleFullscreen: function () {
-            if (!this.fullscreen)
+            this.isFullscreen = !this.isFullscreen;
+            if (this.isFullscreen)
                 util.openFullscreen();
             else
                 util.closeFullscreen();
-        },
-
-        _onFullscreenChange: function (e) {
-            this.fullscreen = !this.fullscreen;
         },
     };
 })();
