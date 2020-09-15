635043e46ff9a711c4cdac4a7ea98e02a53570c7
Mark Tibblin <mark.tibblin@gmail.com>
2020-09-15 13:40:29 +0200

    Let js handle the menu's fullscreen button

---
+++ Bostadsvaljare/wwwroot/scripts/bv.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    window.bv = {
+        fullscreen: false,
+        listeners: {},
+
+        initialize: function () {
+            var self = this;
+
+            this.listeners.fullscreenchange = function(e) { self._onFullscreenChange(e); };
+            this.listeners.toggleFullscreen = function() { self.toggleFullscreen(); };
+
+            document.addEventListener('fullscreenchange', this.listeners.fullscreenchange, false);
+            $('#bv-fullscreen-btn').on('click', this.listeners.toggleFullscreen);
+        },
+
+        toggleFullscreen: function () {
+            if (!this.fullscreen)
+                util.openFullscreen();
+            else
+                util.closeFullscreen();
+        },
+
+        _onFullscreenChange: function (e) {
+            this.fullscreen = !this.fullscreen;
+        },
+    };
+})();
