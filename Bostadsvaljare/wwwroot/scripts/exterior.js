c3b393abdd6095a266e86fd8854d10743cefdb4b
Mark Tibblin <mark.tibblin@gmail.com>
2020-09-20 14:48:53 +0200

    Added image preloader

---
+++ Bostadsvaljare/wwwroot/scripts/exterior.js
@@ -0,0 +1,80 @@
+﻿(function () {
+    window.exterior = {
+        images: [],
+        mapIndex: {},
+
+        loadImages: async function (data) {
+            var self = this,
+                imagesLoaded = data.length * 3,
+                onLoadImg = function () { imagesLoaded = imagesLoaded - 1; };
+            // Load images that use image maps every time
+            // TODO: fix so it's not neccessary to load every time;
+            //       some bug causes only some image maps to load otherwise
+            data.forEach((view, i) => {
+                for (var name in view.sunStudies) {
+                    var source = view.sunStudies[name],
+                        img = new Image();
+                    img.onload = onLoadImg;
+                    img.id = 'sun-study-' + i + '-' + name + '-img';
+                    img.src = source;
+                    $(img).attr({
+                        view: i,
+                        'sun-study': name,
+                    });
+                    self.images.push({
+                        img: img,
+                        parentID: '#view-' + i + ' #sun-study-' + name,
+                        usemap: '#houses-' + i + '-' + name,
+                        style: { width: '99%' },
+                    });
+                }
+            });
+
+            var i, name, ind = 0;
+            for (i in data.view) {
+                for (name in data.view[i].sunStudies) {
+                    this.mapIndex[name + i] = ind++;
+                }
+            }
+
+            while (imagesLoaded > 0) {
+                await util.delay(100);
+            }
+            return true;
+        },
+
+        applyImages: async function () {
+            var data;
+
+            for (data of this.images) {
+                // Reset any style that may have been added previously,
+                // and add in our own
+                $(data.img).removeAttr('style');
+                $(data.img).css(data.style);
+                // Add img element
+                $(data.parentID).append(data.img);
+                // Add image map functionality
+                $('#' + data.img.id).attr('usemap', data.usemap);
+                this._loadIM(data.img);
+            }
+            await util.delay(100);
+        },
+
+        _loadIM: function (img) {
+            var view = $(img).attr('view'),
+                sunStudy = $(img).attr('sun-study'),
+                imgWidth = $(img).css('width'),
+                parentID = 'view-' + view + ' #sun-study-' + sunStudy,
+                imgID = 'sun-study-' + view + '-' + sunStudy + '-img',
+                mapName = 'houses-' + view + '-' + sunStudy;
+
+            mapster_responsive.setValues(this.mapIndex[sunStudy+view], parentID, imgWidth);
+            mapster.addMapHighlights(parentID, imgID, mapName, 'center');
+        },
+
+        dispose: function () {
+            mapster.dispose();
+            this.images.length = 0;
+        },
+    };
+})();
