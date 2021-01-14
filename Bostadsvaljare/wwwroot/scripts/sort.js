b1fd39dde0266c13b050a34934eae0f2addf5f33
Philip Jacobson <Philip@Visivo.se>
2021-01-14 18:10:25 +0100

    added sort function to lgh lista and updated css

---
+++ Bostadsvaljare/wwwroot/scripts/sort.js
@@ -0,0 +1,63 @@
+﻿function sortTabletest(n) {
+    var table1, rows, switching, i, x, y, shouldSwitch, dir, switchCount = 0;
+
+    table1 = document.getElementById("tabletest");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table1.rows;
+
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            if (n == 3) {
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+
+                    }
+
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+
+                    }
+                }
+
+
+            }
+
+            if (dir == "asc") {
+                if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+                    shouldSwitch = true;
+                    break;
+
+                }
+
+            } else if (dir == "desc") {
+                if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+                    shouldSwitch = true;
+                    break;
+
+                }
+
+            }
+
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchCount++;
+        } else {
+            if (switchCount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+
+}
