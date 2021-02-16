b24c5338217561dbdc78a0b41e661903630f11dc
Philip Jacobson <Philip@Visivo.se>
2021-02-16 22:59:04 +0100

    working on sunstudy

--- Bostadsvaljare/wwwroot/scripts/Video.js
+++ Bostadsvaljare/wwwroot/scripts/Video.js
@@ -1,59 +1,70 @@
-﻿function Reverse(id, btnid) {
-    var els = document.getElementsByClassName('full')
-    var video = document.getElementById(id)
-    Array.prototype.forEach.call(els, function (el) {
-        el.classList.add('hide')
-        console.log(el.tagName);
-    });
-   
-    video.classList.add('show')
-    video.play()
-    video.onended = function () {
-        video.classList.remove('show')
-        document.getElementById('img1').classList.add('show')
-    }
+﻿var curIdx = 1;
+var target = 1;
+var direction = 0;
+var vid;
+var img;
 
-    var currentimg = document.getElementsByClassName('show')[0]
+function playsunstudy(id, btnid) {
+    // direction=1(forward), direction=-1(backward), direction=0(replay)
+    direction = (btnid == curIdx) ? 0 : ((btnid > curIdx) ? 1 : -1);
 
-    var Numberofloops = btnid - currentimg
+    target = btnid;
 
-    
-
-    if (Numberofloops > 0) {
-
-        while (i < Numberofloops) {
-            text += "The number is " + i;
-            i++;
-        }
-    }
-    else {
-
-        var Numberofloops2 = Numberofloops * -1;
-
-        while (i < Numberofloops2) {
-           
-            id++;
-            i++;
-            looping(id, i)
-        }
-
-    }
-
-
-    function looping(id, i, numberofloops) {
-        Array.prototype.forEach.call(els, function (el) {
-            el.classList.add('hide')
-            console.log(el.tagName);
-        });
-
-        var vid = document.getElementById('myVideo' + `${id}`)
-        vid.classList.add('show')   
-        vid.play()
-        vid.onended = function () {
-           
-            video.classList.remove('show')
-            document.getElementById('img'+`${i}`).classList.add('show')
-        }
+    if (!direction) {
+        playVid(id);
+    } else {
+        curIdx += direction;
+        playVid('myVideo' + `${curIdx}`);
     }
 }
 
+function playVid(id) {
+    // remove event listener
+    var videoes = document.getElementsByTagName("video");
+    Array.prototype.forEach.call(videoes, function (video) {
+        video.removeEventListener('ended', myHandler, false)
+    });
+
+    vid = document.getElementById(id);
+    img = document.getElementById(curIdx);
+
+    var els = document.getElementsByClassName('full')
+    Array.prototype.forEach.call(els, function (el) {
+        el.classList.remove('show')
+        el.classList.add('hide')
+    });
+
+    img.classList.remove('hide');
+    img.classList.add('show');
+
+    setTimeout(() => {
+        img.classList.remove('show');
+        img.classList.add('hide');
+        vid.classList.remove('hide');
+        vid.classList.add('show');
+        vid.currentTime = 0;
+        vid.play();
+        vid.addEventListener('ended', myHandler, false);
+    }, 500);
+}
+
+function myHandler(e) {
+    vid.currentTime = 0;
+    vid.classList.remove('show');
+    vid.classList.add('hide');
+    img.classList.remove('hide');
+    img.classList.add('show');
+
+    if (curIdx != target) {
+        curIdx += direction;
+        playVid('myVideo' + `${curIdx}`);
+    }
+
+    else if (curIdx == 5) {
+        img.classList.remove('show');
+        img.classList.add('hide');
+
+        document.getElementById('6').classList.remove('hide');
+        document.getElementById('6').classList.add('show');
+    }
+}
---
+++ Bostadsvaljare/wwwroot/scripts/solution.js
@@ -0,0 +1,73 @@
+<script type="text/javascript">
+        var curIdx = 1;
+        var target = 1;
+        var direction = 0;
+        var vid;
+        var img;
+
+        function playsunstudy(id, btnid) {
+           // direction=1(forward), direction=-1(backward), direction=0(replay)
+           direction = (btnid==curIdx) ? 0 : ((btnid>curIdx)? 1 : -1);
+
+           target = btnid;
+           
+            if(!direction) {
+                playVid(id);
+           } else {
+                curIdx += direction;
+                playVid('myVideo' + `${curIdx}`);
+           }
+        }
+
+        function playVid(id) {
+            // remove event listener
+            var videoes = document.getElementsByTagName("video");
+            Array.prototype.forEach.call(videoes, function (video) {
+                video.removeEventListener('ended', myHandler, false)
+            });
+
+            vid = document.getElementById(id);
+            img = document.getElementById(curIdx);
+            
+            var els = document.getElementsByClassName('full')
+            Array.prototype.forEach.call(els, function (el) {
+                el.classList.remove('show')
+                el.classList.add('hide')
+            });
+            
+            img.classList.remove('hide');
+            img.classList.add('show');
+            
+            setTimeout(()=> {
+                img.classList.remove('show');
+                img.classList.add('hide');
+                vid.classList.remove('hide');
+                vid.classList.add('show');
+                vid.currentTime=0;
+                vid.play();
+                vid.addEventListener('ended',myHandler,false);
+            }, 500);
+        }
+
+        function myHandler(e) {
+            vid.currentTime=0;
+            vid.classList.remove('show');
+            vid.classList.add('hide');
+            img.classList.remove('hide');
+            img.classList.add('show');
+
+            if(curIdx != target) {
+                curIdx += direction;
+                playVid('myVideo' + `${curIdx}`);
+            }
+
+            else if(curIdx == 5) {
+                img.classList.remove('show');
+                img.classList.add('hide');
+
+                document.getElementById('6').classList.remove('hide');
+                document.getElementById('6').classList.add('show');
+            }
+        }
+
+    </script>
